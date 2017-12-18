fcbfc4b1cd5a7f2b05565437a73d1e57e6f4d027
Manokero <dancebreak29@gmail.com>
2017-12-17 20:20:35 -0400

    rename folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ var minify = require('gulp-minify');
 
 
 gulp.task('minify', function() {
-  return gulp.src('asserts/views/*.html')
+  return gulp.src('assets/views/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('public'));
 });
 
 gulp.task('minify-css', function(){
-  return gulp.src('asserts/styles/*.css')
+  return gulp.src('assets/styles/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('public'));
 });
 
 gulp.task('minify-js', function() {
-  gulp.src('asserts/js/*.js')
+  gulp.src('assets/js/*.js')
     .pipe(minify())
     .pipe(gulp.dest('public'))
 });
